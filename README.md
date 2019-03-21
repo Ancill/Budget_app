8c9f4115be9cb044bbee15de2c24675a6aca20df
ivanfromspace <ivanfromspace@gmail.com>
2019-03-21 13:44:50 +0300

    add percentage calc

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 This is app from The complete JS course 
---- App written in vannila JS without any frameworks
+
+#App written in vannila JS without any frameworks
+
+
+###Last update
+-Created delete button and implement delete function with updating UI and whole budget summary
--- app.js
+++ app.js
@@ -5,7 +5,20 @@ var budgetController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
     }
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome )* 100)
+
+        } else {
+            this.percentage = -1;
+        }
+    }
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
+    }
+
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
@@ -86,6 +99,21 @@ var budgetController = (function () {
             }
 
             //expense = 100 and income 200, spent 33.33% = 100/200 *100
+        },
+
+        calculatePercentages: function() {
+            data.allItems.exp.forEach(function(cur) {
+                cur.calcPercentage();
+
+            })
+        },
+
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur) {
+                return cur.getPercentage();
+
+            })  
+            return allPerc;
         },
         // last function to returning data for implementation in DOM/ call it after calculateBudget();
         getBudget: function() {
@@ -237,6 +265,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         UICtrl.displayBudget(budget);
     }
 
+    var updatePercentages = function() {
+        //1. Calculate percentages
+        budgetCtrl.calculatePercentages();
+
+        //2. Read percentages from budget controller
+        var percentages = budgetCtrl.getPercentages();
+
+        //3. Update UI with new percentages
+        console.log(percentages);
+    }
 
     var ctrlAddItem = function() {
         var input, newItem;
@@ -256,6 +294,9 @@ var controller = (function(budgetCtrl, UICtrl) {
             
             // 5. Calculate and update budget
             updateBudget();
+
+            //6. Update perentages
+            updatePercentages();
         }
     }
 
@@ -279,6 +320,8 @@ var controller = (function(budgetCtrl, UICtrl) {
             updateBudget();
         }
     }
+
+    
     return {
         init: function() {
             console.log('Application has started');
